ba57e4eb421605794bfdb20cc267851d49c6ab24
JAICHANGPARK <aristojeff@gmail.com>
2016-08-02 13:50:29 +0900

    Update ViewController.swift

--- Loginapp/ViewController.swift
+++ Loginapp/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import Firebase
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // 버튼만들기
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .Plain, target: self, action: #selector(handleLogout))
+        
+        if FIRAuth.auth()?.currentUser?.uid == nil{
+            //handleLogout()
+            performSelector(#selector(handleLogout), withObject: nil, afterDelay: 0)
+            
+            
+        }
+        
+        //  let ref = FIRDatabase.database().referenceFromURL("https://loginapp-3f5ea.firebaseio.com/")
+        // ref.updateChildValues(["someValue":123123])
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func handleLogout(){
+        
+        do {
+            
+            try FIRAuth.auth()?.signOut()
+        } catch let logoutError{
+            
+            print(logoutError)
+            
+        }
+        
+        let loginController = LoginController()
+        
+       presentViewController(loginController, animated: true, completion: nil)
+        
+        
+        
     }
-
 
 }
 
